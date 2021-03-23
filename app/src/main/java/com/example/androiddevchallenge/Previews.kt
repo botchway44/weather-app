61b4c5062f798c2a03cfecec3ab2eef9fffcdbaa
botchway44 <noelnuel44@gmail.com>
2021-03-23 20:39:44 +0000

    cleanup spotless errors

--- app/src/main/java/com/example/androiddevchallenge/Previews.kt
+++ app/src/main/java/com/example/androiddevchallenge/Previews.kt
@@ -19,6 +19,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
+
+
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
@@ -26,6 +28,8 @@ fun LightPreview() {
         MyApp()
     }
 }
+
+
 
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
